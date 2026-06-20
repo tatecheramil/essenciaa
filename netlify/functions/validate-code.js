2defbb5764bd783959992ffd3876fa44cb21eebc
tatecheramil <marga_189@hotmail.com>
2026-06-20 14:38:12 +0100

    Add files via upload

--- netlify/functions/validate-code.js
+++ netlify/functions/validate-code.js
@@ -32,7 +32,11 @@ exports.handler = async function (event, context) {
     }
 
     const { getStore } = require('@netlify/blobs');
-    const store = getStore('essencia-codigos');
+    const store = getStore({
+      name: 'essencia-codigos',
+      siteID: process.env.NETLIFY_SITE_ID,
+      token: process.env.NETLIFY_BLOBS_TOKEN
+    });
 
     let registo = null;
     try {
